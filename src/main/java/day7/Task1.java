68f230d4c88f8e6783d5c12a3f2fe81e2ef1676b
IrinaSakhnovskaya <sakhnovskaya@mail.ru>
2022-12-07 23:23:43 +1000

    complete day7 tasks

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -1,7 +1,11 @@
 package day7;
 
+
 public class Task1 {
     public static void main(String[] args) {
+        Airplane airplane1 = new Airplane(" ",1990, 20, 30000, 1000);
+        Airplane airplane2 = new Airplane(" ", 1987, 33, 32000, 2000);
 
-    }
+        Airplane.compareAirplanes(airplane1, airplane2);
+}
 }
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -1,7 +1,35 @@
-package day7;
+
+ package day7;
+
+import java.util.Random;
 
 public class Task2 {
     public static void main(String[] args) {
 
+        Random random = new Random();
+
+        Player player1 = new Player(random.nextInt(10) - 100);
+        Player player2 = new Player(random.nextInt(10) - 100);
+        Player player3 = new Player(random.nextInt(10) - 100);
+
+        Player.info();
+
+        Player player4 = new Player(random.nextInt(10) - 100);
+        Player player5 = new Player(random.nextInt(10) - 100);
+
+        Player.info();
+
+        Player player6 = new Player(random.nextInt(10) - 100);
+
+        Player.info();
+
+        Player player7 = new Player(25);
+
+        Player.info();
+
+        for (int i = 0; i > player2.stamina; i++)
+            player2.run();
+
+        Player.info();
     }
 }
